fa9e208217cba4a85feeaf21641cc9ee8364cadf
Балабанов Александр Дмитриевич <ip312s02@csc.sibsutis.ru>
2026-02-14 19:00:51 +0700

    Add User Record

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how OpenIDE suggests fixing it.
-    IO.println("Hello and welcome!");
+    try {
+        User u1 = User.create("Sasha", "Balabanov", "SashaB_312@mail.ru");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        System.out.println(u1.format());
+    } catch (Exception e) {
+        IO.println(e.getMessage());
     }
 }
