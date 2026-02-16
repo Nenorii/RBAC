6e38c1fbf1adcd934ea8daa9fa803d31095555fe
Балабанов Александр Дмитриевич <ip312s02@csc.sibsutis.ru>
2026-02-16 19:03:18 +0700

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,30 @@
 void main() {
-    try {
-        User u1 = User.create("Sasha", "Balabanov", "SashaB_312@mail.ru");
+        User user1 = new User("username", "full name", "email@gmail.com");
+        User user2 = new User("operator", "full name", "email@gmail.com");
 
-        Permission read = new Permission("READ", "users", "Может читать пользователей");
-        Permission write = new Permission("WRITE", "reports", "Может писать отчеты");
-        Permission delete = new Permission("DELETE", "settings", "Может удалять настройки");
+        Role role = new Role("admin", "admin permissions");
 
-        System.out.println("\nИнформация о правах:");
-        System.out.println(read.format());
-        System.out.println(write.format());
-        System.out.println(delete.format());
+        Permission read = new Permission("READ", "users", "Can read users");
+        Permission write = new Permission("WRITE", "reports", "Can write reports");
+        Permission delete = new Permission("DELETE", "settings", "Can delete users");
 
-        System.out.println("\nПоиск 'RE' в name и 'use' в resource:");
-        System.out.println("READ: " + read.matches("RE", "use"));
-        System.out.println("WRITE: " + write.matches("RE", "use"));
-        System.out.println("DELETE: " + delete.matches("RE", "use"));
+        role.addPermission(read);
+        role.addPermission(write);
+        role.addPermission(delete);
 
-        System.out.println("\nПоиск 'WR' в name и 'rep' в resource:");
-        System.out.println("READ: " + read.matches("WR", "rep"));
-        System.out.println("WRITE: " + write.matches("WR", "rep"));
-        System.out.println("DELETE: " + delete.matches("WR", "rep"));
+        AssignmentMetadata am = AssignmentMetadata.now(user2.username(), "with some reason");
 
-        System.out.println("\nПоиск 'DE' в name и 'set' в resource:");
-        System.out.println("READ: " + read.matches("DE", "set"));
-        System.out.println("WRITE: " + write.matches("DE", "set"));
-        System.out.println("DELETE: " + delete.matches("DE", "set"));
+        PermanentAssignment pa = new PermanentAssignment(user1, role, am);
+        System.out.println(pa.summary());
 
-        System.out.println(u1.format());
-    } catch (Exception e) {
-        IO.println(e.getMessage());
-    }
+        pa.revoke();
+        System.out.println(pa.summary());
+
+        TemporaryAssignment ta = new TemporaryAssignment(user1, role, am);
+
+        ta.extend(LocalDate.parse("2035-09-20").atStartOfDay().toString());
+        System.out.println(ta.summary());
+
+        ta.extend(LocalDate.parse("2005-09-20").atStartOfDay().toString());
+        System.out.println(ta.summary());
 }
